0acc2765c626dd0b08ceaef1d16239e3a29078a5
Jeffrey.S.Whitaker <Jeffrey.S.Whitaker@noaa.gov>
2023-12-06 13:36:56 -0500

    clean up logs before writing to s3

--- clean.sh
+++ clean.sh
@@ -3,6 +3,7 @@ cd $datapath2
 /bin/rm -f sfg*control bfg*control
 /bin/rm -f fort*
 /bin/rm -f *log
+/bin/rm -f logs/nc_diag* logs/run_gsi_anal.log logs/clean.out
 /bin/rm -rf *tmp*
 YYYYMMDD=`echo $analdate | cut -c1-8`
 HH=`echo $analdate | cut -c9-10`
